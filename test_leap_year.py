eb792afa7e87e3d7002542b63abad5d3aa546b28
Anime-Quatroctus <kanadetachibona@gmail.com>
2021-05-20 16:44:21 -0700

    More tests for more coverage.

--- test_leap_year.py
+++ test_leap_year.py
@@ -9,5 +9,11 @@ class TestLeapYear(unittest.TestCase):
     def test_not_leap_year(self):
         self.assertFalse(validated_is_leap_year("2001"))
 
+    def test_type_error(self):
+        self.assertRaises(TypeError, validated_is_leap_year, "hello")
+
+    def test_value_error(self):
+        self.assertRaises(ValueError, validated_is_leap_year, "-2000")
+
 if __name__ == "__main__":
     unittest.main()
